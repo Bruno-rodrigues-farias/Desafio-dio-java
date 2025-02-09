af62a62f5716ae0c02f48dd7cf3627b1db7409c2
bruno <brunorodrigues1616@gmail.com>
2025-02-08 21:08:53 -0300

    primeiro

---
+++ src/JavaTeste/Teste.java
@@ -0,0 +1,8 @@
+package JavaTeste;
+
+public class Teste {
+
+    public void Falar(){
+        System.out.println("Olá, mundo");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Olá, mundo");
+        System.out.println("Enviando");
+        System.out.println("Enviando");
+    }
+}
+
